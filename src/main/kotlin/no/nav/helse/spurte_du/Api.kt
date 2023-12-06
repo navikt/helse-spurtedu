01e465374618d0d70f85a3a840bd827d9ad80cfb
david steinsland <david@davidsteinsland.net>
2023-12-06 17:36:56 +0100

    fikser miljøvariabler

--- src/main/kotlin/no/nav/helse/spurte_du/Api.kt
+++ src/main/kotlin/no/nav/helse/spurte_du/Api.kt
@@ -3,20 +3,21 @@ package no.nav.helse.spurte_du
 import redis.clients.jedis.DefaultJedisClientConfig
 import redis.clients.jedis.HostAndPort
 import redis.clients.jedis.JedisPool
+import java.net.URI
 
 fun api(env: Map<String, String>, logg: Logg) {
-    val host = env.getValue("REDIS_HOST")
+    val uri = URI(env.getValue("REDIS_URI_OPPSLAG"))
     val config = DefaultJedisClientConfig.builder()
-        .user(env.getValue("REDIS_USER"))
-        .password(env.getValue("REDIS_PASSWORD"))
+        .user(env.getValue("REDIS_USERNAME_OPPSLAG"))
+        .password(env.getValue("REDIS_PASSWORD_OPPSLAG"))
         .ssl(true)
         .hostnameVerifier { hostname, session ->
-            val evaluering = hostname == host
+            val evaluering = hostname == uri.host
             logg.info("verifiserer vertsnavn $hostname: {}", evaluering)
             evaluering
         }
         .build()
-    JedisPool(HostAndPort(host, env.getValue("REDIS_PORT").toInt()), config).resource.use { jedis ->
+    JedisPool(HostAndPort(uri.host, uri.port), config).resource.use { jedis ->
         jedis.set("foo", "100")
         logg.info("Lagret verdi til redis")
     }
